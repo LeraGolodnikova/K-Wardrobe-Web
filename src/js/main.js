ddda666fea014ef3c76d9ecdb5760f14b01f6e29
LeraGolodnikova <otmach366@gmail.com>
2024-11-03 20:26:00 +0300

    aaaaaa

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("burger").addEventListener("click", function()
+    {
+        document.querySelector(".header_wrapper").classList.toggle("open")
+
+    })
+})
